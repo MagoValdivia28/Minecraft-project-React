39705e06a84e4e4dfff7784d99ef800ca7175733
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2023-12-01 09:21:53 -0300

    Refactor bookPopUp component and page styles

--- app/components/bookPopUp/bookpopup.jsx
+++ app/components/bookPopUp/bookpopup.jsx
@@ -2,9 +2,11 @@ import stlyes from './bookpopup.module.css';
 
 const BookPopUp = ({ handleBookPopUp }) => {
     return (
-        <div className={stlyes.book_main_container}>
-            <button onClick={() => handleBookPopUp()} className={stlyes.book_close_button}>X</button>
-            <p>Vc foi para a pag de PopUp :D</p>
+        <div className={stlyes.main_container}>
+            <div className={stlyes.book_container}>
+
+            </div>
+            <button onClick={() => handleBookPopUp()} className={stlyes.exitButton}>Feito</button>
         </div>
     )
 }
--- app/encantamentos/page.jsx
+++ app/encantamentos/page.jsx
@@ -31,72 +31,76 @@ const Page_de_encantamentos = () => {
                 <h2 className={styles.subTitles}>Encantamentos</h2>
             </div>
             <div className={styles.main_container}>
+
                 {
                     bookPopUp == false ? (
-                        <nav className={styles.encantamentos_container}>
-                            <img className={styles.encantamentos_img} src="/Book_29.webp" alt="encantamento1" width={96} height={96} />
-                            <ul className={styles.encantamentos_list}>
+                        <div className={styles.books_list}>
+                            <nav className={styles.encantamentos_container}>
+                                <img className={styles.encantamentos_img} src="/Book_29.webp" alt="encantamento1" width={96} height={96} />
+                                <ul className={styles.encantamentos_list}>
 
 
-                                <li onClick={() => handleBookPopUp()} className={styles.encantamento}>
-                                    <span className={styles.book}>
-                                        <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
-                                        <p className={styles.book_name}>inquebravel</p>
-                                    </span>
-                                </li>
+                                    <li onClick={() => handleBookPopUp()} className={styles.encantamento}>
+                                        <span className={styles.book}>
+                                            <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
+                                            <p className={styles.book_name}>inquebravel</p>
+                                        </span>
+                                    </li>
 
-                                <li onClick={() => handleBookPopUp()} className={styles.encantamento}>
-                                    <span className={styles.book1}>
-                                        <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
-                                        <p className={styles.book_name}>protecao</p>
-                                    </span>
-                                </li>
+                                    <li onClick={() => handleBookPopUp()} className={styles.encantamento}>
+                                        <span className={styles.book1}>
+                                            <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
+                                            <p className={styles.book_name}>protecao</p>
+                                        </span>
+                                    </li>
 
-                                <li onClick={() => handleBookPopUp()} className={styles.encantamento}>
-                                    <span className={styles.book2}>
-                                        <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
-                                        <p className={styles.book_name}>inquebravel</p>
-                                    </span>
-                                </li>
-                                <li onClick={() => handleBookPopUp()} className={styles.encantamento}>
-                                    <span className={styles.book3}>
-                                        <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
-                                        <p className={styles.book_name}>inquebravel</p>
-                                    </span>
-                                </li>
-                                <li onClick={() => handleBookPopUp()} className={styles.encantamento}>
-                                    <span className={styles.book4}>
-                                        <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
-                                        <p className={styles.book_name}>inquebravel</p>
-                                    </span>
-                                </li>
-                                <li onClick={() => handleBookPopUp()} className={styles.encantamento}>
-                                    <span className={styles.book5}>
-                                        <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
-                                        <p className={styles.book_name}>Respiracao aquatica</p>
-                                    </span>
-                                </li>
+                                    <li onClick={() => handleBookPopUp()} className={styles.encantamento}>
+                                        <span className={styles.book2}>
+                                            <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
+                                            <p className={styles.book_name}>inquebravel</p>
+                                        </span>
+                                    </li>
+                                    <li onClick={() => handleBookPopUp()} className={styles.encantamento}>
+                                        <span className={styles.book3}>
+                                            <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
+                                            <p className={styles.book_name}>inquebravel</p>
+                                        </span>
+                                    </li>
+                                    <li onClick={() => handleBookPopUp()} className={styles.encantamento}>
+                                        <span className={styles.book4}>
+                                            <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
+                                            <p className={styles.book_name}>inquebravel</p>
+                                        </span>
+                                    </li>
+                                    <li onClick={() => handleBookPopUp()} className={styles.encantamento}>
+                                        <span className={styles.book5}>
+                                            <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
+                                            <p className={styles.book_name}>Respiracao aquatica</p>
+                                        </span>
+                                    </li>
 
 
-                            </ul>
-                        </nav>
+                                </ul>
+                            </nav>
+                            <div className={styles.arrows_container}>
+                                <button className={styles.arrows}>
+                                    <MdArrowBackIos />
+
+                                </button>
+                                <h2>
+                                    Pag 1 / 1
+                                </h2>
+                                <button className={styles.arrows}>
+                                    <MdArrowForwardIos />
+                                </button>
+                            </div>
+                        </div>
                     ) : (
-                        <BookPopUp handleBookPopUp = {handleBookPopUp} />
+                        <BookPopUp handleBookPopUp={handleBookPopUp} />
                     )
                 }
 
-                <div className={styles.arrows_container}>
-                    <button className={styles.arrows}>
-                        <MdArrowBackIos />
 
-                    </button>
-                    <h2>
-                        Pag 1 / 1
-                    </h2>
-                    <button className={styles.arrows}>
-                        <MdArrowForwardIos />
-                    </button>
-                </div>
             </div>
         </main>
 
