3410e67a3a7cdd8c45aead5c5e4be2a5756fd39d
felipe pedro <felipepedro.senai564@gmail.com>
2023-12-04 10:04:06 -0300

    Add Batalha page component

---
+++ app/batalha/page.jsx
@@ -0,0 +1,20 @@
+"use client"
+import Style from './batalha.module.css';
+
+const Batalha = () => {
+    return (
+        <main>
+            <div>
+                <div>
+                    
+                </div>
+
+                    <div>BATALHE</div>
+
+                <div>
+
+                </div>
+            </div>
+        </main>
+    );
+}
