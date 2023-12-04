2113bfbf9e504c33f7264f10efc64d24a06a28b2
Pedro <pedrosouza.senai564@gmail.com>
2023-12-04 10:33:10 -0300

    Add styles and content to the membros page

--- app/membros/page.jsx
+++ app/membros/page.jsx
@@ -6,12 +6,66 @@ import { useRouter } from 'next/navigation';
 
 const membroPage = () => {
     return (
-        <div>
+        <>
             <Header />
-            <div className={styles.container}>
-                <h1>Membros</h1>
+            <div className={styles.main}>
+
+                <h1 className={styles.textoHeader}>Membros</h1>
+                <div className={styles.sobreEquipe}>
+                    <div className={styles.imgEquipe}>
+                        <img className={styles.img1} src="/enigma.jpg" />
+                    </div>
+                    <div className={styles.textoEquipe}>
+                        <p>É com grande satisfação que apresentamos a equipe de desenvolvimento de sistemas ENIGMA, dedicada e altamente desenvolvida na concepção e implementação de soluções tecnológicas inovadoras. Nossa equipe é composta por profissionais especializados, cujo comprometimento e expertise são fundamentais para o sucesso de nossos projetos.
+                            A ENIGMA se destaca pela abordagem colaborativa e multidisciplinar de seus membros, reunindo talentos em áreas como análise de sistemas, programação, design de interfaces e gerenciamento de projetos. Cada membro possui vasta experiência no desenvolvimento de soluções personalizadas, adaptadas às necessidades específicas de nossos clientes.
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.equipe}>
+                    <div className={styles.umAoTres}>
+                        <div className={styles.cardPessoa}>
+                            <img className={styles.imgPessoa} src="/imagemfacemine1.webp" />
+                            <h2 className={styles.pessoa}>Felipe Pedro</h2>
+                            <p>Tech Lead/Desenvolvedor</p>
+                        </div>
+                        <div className={styles.cardPessoa}>
+                            <img className={styles.imgPessoa} src="/imagemfacemine2.jpeg" />
+                            <h2 className={styles.pessoa}>Guilherme Rocha</h2>
+                            <p>Desenvolvedor</p>
+                        </div>
+                        <div className={styles.cardPessoa}>
+                            <img className={styles.imgPessoa} src="/imagemfacemine3.png" />
+                            <h2 className={styles.pessoa}>Matheus Coco</h2>
+                            <p>Desenvolvedor</p>
+                        </div>
+                    </div>
+                    <div className={styles.quatroAoSeis}>
+                        <div className={styles.cardPessoa}>
+                            <img className={styles.imgPessoa} src="/imagemfacemine4.png" />
+                            <h2 className={styles.pessoa}>Matheus Gomes</h2>
+                            <p>Desenvolvedor</p>
+                        </div>
+                        <div className={styles.cardPessoa}>
+                            <img className={styles.imgPessoa} src="/imagemfacemine5.jpg" />
+                            <h2 className={styles.pessoa}>Pedro Isac</h2>
+                            <p>Desenvolvedor</p>
+                        </div>
+                        <div className={styles.cardPessoa}>
+                            <img className={styles.imgPessoa} src="/imagemfacemine6.png" />
+                            <h2 className={styles.pessoa}>Thayna Vazzoler</h2>
+                            <p>Desenvolvedor</p>
+                        </div>
+
+                    </div>
+                    <div className={styles.cardCriarMembro}>
+                        <img className={styles.imgPessoa} src="/" />
+                    </div>
+                </div>
+                <div>
+
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
