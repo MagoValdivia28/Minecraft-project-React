a65ce60e5d2b4e04edacc32195d9e90a9993fe65
Pedro <pedrosouza.senai564@gmail.com>
2023-12-08 11:20:33 -0300

    Refactor CSS and JSX code for member cards

--- app/membros/page.jsx
+++ app/membros/page.jsx
@@ -71,61 +71,57 @@ const membroPage = () => {
                     </div>
                 </div>
                 <div className={styles.equipe}>
-                    <div className={styles.umAoTres}>
-                        <div className={styles.cardPessoa1}>
-                            <img className={styles.imgPessoa} src="/imagemfacemine1.webp" />
-                            <h2 className={styles.pessoa}>Felipe Pedro</h2>
-                            <p>Tech Lead/Desenvolvedor</p>
-                            <div className={styles.botaoVM}>
-                                <button className={styles.botaoVermais}>Detalhes</button>
-                            </div>
-                        </div>
-                        <div className={styles.cardPessoa2}>
-                            <img className={styles.imgPessoa} src="/imagemfacemine2.jpeg" />
-                            <h2 className={styles.pessoa}>Guilherme Rocha</h2>
-                            <p>Desenvolvedor</p>
-                            <div className={styles.botaoVM}>
-                                <button className={styles.botaoVermais}>Detalhes</button>
-                            </div>
-                        </div>
-                        <div className={styles.cardPessoa3}>
-                            <img className={styles.imgPessoa} src="/imagemfacemine3.png" />
-                            <h2 className={styles.pessoa}>Matheus Coco</h2>
-                            <p>Desenvolvedor</p>
-                            <div className={styles.botaoVM}>
-                                <button className={styles.botaoVermais}>Detalhes</button>
-                            </div>
+                    <div className={styles.cardPessoa}>
+                        <img className={styles.imgPessoa} src="/imagemfacemine1.webp" />
+                        <h2 className={styles.pessoa}>Felipe Pedro</h2>
+                        <p>Tech Lead/Desenvolvedor</p>
+                        <div className={styles.botaoVM}>
+                            <button className={styles.botaoVermais}>Detalhes</button>
                         </div>
                     </div>
-                    <div className={styles.quatroAoSeis}>
-                        <div className={styles.cardPessoa4}>
-                            <img className={styles.imgPessoa} src="/imagemfacemine4.png" />
-                            <h2 className={styles.pessoa}>Matheus Gomes</h2>
-                            <p>Desenvolvedor</p>
-                            <div className={styles.botaoVM}>
-                                <button className={styles.botaoVermais}>Detalhes</button>
-                            </div>
+                    <div className={styles.cardPessoa}>
+                        <img className={styles.imgPessoa} src="/imagemfacemine2.jpeg" />
+                        <h2 className={styles.pessoa}>Guilherme Rocha</h2>
+                        <p>Desenvolvedor</p>
+                        <div className={styles.botaoVM}>
+                            <button className={styles.botaoVermais}>Detalhes</button>
                         </div>
-                        <div className={styles.cardPessoa5}>
-                            <img className={styles.imgPessoa} src="/imagemfacemine5.png" />
-                            <h2 className={styles.pessoa}>Pedro Isac</h2>
-                            <p>Desenvolvedor</p>
-                            <div className={styles.botaoVM}>
-                                <button className={styles.botaoVermais}>Detalhes</button>
-                            </div>
+                    </div>
+                    <div className={styles.cardPessoa}>
+                        <img className={styles.imgPessoa} src="/imagemfacemine3.png" />
+                        <h2 className={styles.pessoa}>Matheus Coco</h2>
+                        <p>Desenvolvedor</p>
+                        <div className={styles.botaoVM}>
+                            <button className={styles.botaoVermais}>Detalhes</button>
                         </div>
-                        <div className={styles.cardPessoa6}>
-                            <img className={styles.imgPessoa} src="/imagemfacemine6.png" />
-                            <h2 className={styles.pessoa}>Thayna Vazzoler</h2>
-                            <p>Desenvolvedor</p>
-                            <div className={styles.botaoVM}>
-                                <button className={styles.botaoVermais}>Detalhes</button>
-                            </div>
+                    </div>
+                    <div className={styles.cardPessoa}>
+                        <img className={styles.imgPessoa} src="/imagemfacemine4.png" />
+                        <h2 className={styles.pessoa}>Matheus Gomes</h2>
+                        <p>Desenvolvedor</p>
+                        <div className={styles.botaoVM}>
+                            <button className={styles.botaoVermais}>Detalhes</button>
                         </div>
+                    </div>
+                    <div className={styles.cardPessoa}>
+                        <img className={styles.imgPessoa} src="/imagemfacemine5.png" />
+                        <h2 className={styles.pessoa}>Pedro Isac</h2>
+                        <p>Desenvolvedor</p>
+                        <div className={styles.botaoVM}>
+                            <button className={styles.botaoVermais}>Detalhes</button>
+                        </div>
+                    </div>
+                    <div className={styles.cardPessoa}>
+                        <img className={styles.imgPessoa} src="/imagemfacemine6.png" />
+                        <h2 className={styles.pessoa}>Thayna Vazzoler</h2>
+                        <p>Desenvolvedor</p>
+                        <div className={styles.botaoVM}>
+                            <button className={styles.botaoVermais}>Detalhes</button>
+                        </div>
+                    </div>
 
-                    </div>
                     <div className={styles.cardCriarMembro}>
-
+                        <button className={styles.botaoAdd}>+</button>
                     </div>
                 </div>
                 <div>
