9163671c75b7af1eee012fc000c355d53e972e7c
guilegal <guilhermeodorissio.564@gmail.com>
2023-12-11 16:54:53 -0300

    Refactor mob route and add styling to mob list

--- app/api/mobs/[id]/route.js
+++ app/api/mobs/[id]/route.js
@@ -2,27 +2,23 @@ import axios from "axios";
 
 import { NextResponse } from "next/server";
 
-export async function PUT(request) {
-    const params = await request.json();
-
+export async function GET() {
     try {
-        const response = await axios.put("http://localhost:4000/mobs", params);
-        return NextResponse.json(response.data);
-    }
-    catch (error) {
-        console.log("[ORDER_PUT]", error);
+        const response = await axios.get(`http://localhost:4000/mobs/${id}`);
+        return NextResponse.json(response.data.mobs);
+    } catch (error) {
+        console.log("[ORDER_GET]", error);
         return new NextResponse("Erro interno do servidor!", { status: 500 });
     }
 }
 
-export async function DELETE(request) {
-    const params = await request.json();
-    console.log("AAAAAAAAAAAAA");
-    console.log(params.id);
+export async function DELETE(request, { params }) {
+    const { id } = params;
 
     try {
-        const response = await axios.delete(`http://localhost:4000/mobs` + params.id);
+        const response = await axios.delete(`http://localhost:4000/mobs/${id}`);
         return NextResponse.json(response.data);
+        
     }
     catch (error) {
         console.log("[ORDER_DELETE]", error);
