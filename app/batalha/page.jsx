3ca92c44f4c0e56954ab8fa2d3a52af0bbeb7273
pravel <123407140+ElMatheus@users.noreply.github.com>
2023-12-13 11:04:28 -0300

    fit: css card

--- app/batalha/page.jsx
+++ app/batalha/page.jsx
@@ -58,40 +58,44 @@ const Batalha = () => {
                 </div>
 
                 <div className={Style.batalhaConteiner}>
-                    <div className={StyleSheet.gerenciamentoEquipamento}>
-
+                    <div className={Style.gerenciamentoEquipamento}>
                         <div>
                             <button className={Style.createButton} onClick={() => setButtonEscolha(true)}>ESCOLHA SEU EQUIPAMENTO</button>
                         </div>
-                        <div className={Style.equipamentosSelecionados}>
-                            <div className={Style.itemArmadura}>
-                                <img style={{ background: selecionarCapacete?.cor }} src={`inventory/${selecionarCapacete?.tipo}Final.png`} alt="" />
-                            </div>
-                            <div className={Style.itemArmadura}>
-                                <img style={{ background: selecionarPeitoral?.cor }} src={`inventory/${selecionarPeitoral?.tipo}Final.png`} alt="" />
-                            </div>
-                            <div className={Style.itemArmadura}>
-                                <img style={{ background: selecionarCalca?.cor }} src={`inventory/${selecionarCalca?.tipo}Final.png`} alt="" />
-                            </div>
-                            <div className={Style.itemArmadura}>
-                                <img style={{ background: selecionarBota?.cor }} src={`inventory/${selecionarBota?.tipo}Final.png`} alt="" />
-                            </div>
-                            <div className={Style.itemArmadura}>
-                                <img style={{ background: selecionarEspada?.cor }} src={`inventory/${selecionarEspada?.tipo}Final.png`} alt="" />
+                        <div className={Style.containerAll}>
+                            <img src="https://www.minecraft.net/content/dam/games/minecraft/key-art/Play_With_Friends_Online_672x360.png" alt="" />
+                            <div className={Style.containerAttachmentEquipamentos}>
+                                <div className={Style.containerEquipamentos}>
+                                    <div className={Style.itemArmadura}>
+                                        <img style={{ background: selecionarCapacete?.cor }} src={`inventory/${selecionarCapacete?.tipo}Final.png`} alt="" />
+                                    </div>
+                                    <div className={Style.itemArmadura}>
+                                        <img style={{ background: selecionarPeitoral?.cor }} src={`inventory/${selecionarPeitoral?.tipo}Final.png`} alt="" />
+                                    </div>
+                                    <div className={Style.itemArmadura}>
+                                        <img style={{ background: selecionarCalca?.cor }} src={`inventory/${selecionarCalca?.tipo}Final.png`} alt="" />
+                                    </div>
+                                    <div className={Style.itemArmadura}>
+                                        <img style={{ background: selecionarBota?.cor }} src={`inventory/${selecionarBota?.tipo}Final.png`} alt="" />
+                                    </div>
+                                </div>
+                                <div className={Style.containerEspada}>
+                                    <div className={Style.itemArmadura}>
+                                        <img style={{ background: selecionarEspada?.cor }} src={`inventory/${selecionarEspada?.tipo}Final.png`} alt="" />
+                                    </div>
+                                    <div className={Style.status}>
+                                        <span className={`${Style.textop} ${Style.dano}`}>ataque: {selecionarEspada ? selecionarEspada.dano : 0}</span>
+                                        <span className={`${Style.textop} ${Style.defesa}`}>defesa: {defesa}</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div className={Style.status}>
-                            <span className={`${Style.textop} ${Style.dano}`}>ataque: {selecionarEspada ? selecionarEspada.dano : 0}</span>
-                            <span className={`${Style.textop} ${Style.defesa}`}>defesa: {defesa}</span>
-                        </div>
-                    </div>
 
+                    </div>
 
                     <div>
                         <img className={Style.espadaX} src={"espadasX.png"} alt="" />
                     </div>
-
-
                     <div className={Style.mob}>
                         <p className={Style.textop}>SELECIONE O MOB PARA ENFRENTA-LO!</p>
                     </div>
