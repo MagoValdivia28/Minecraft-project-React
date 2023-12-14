74aa06bb1ffa2427a5723e721cb520b774343952
ElMatheus <matheus.goncalves5@aluno.senai.br>
2023-12-14 19:30:49 -0300

    fit: header responsivo

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -1,21 +1,43 @@
+"use client"
+
 import styles from './header.module.css';
 import Link from 'next/link';
+import { LiaBarsSolid } from "react-icons/lia";
+import { useState } from 'react';
 
-const Header = () => (
-    <header className={styles.header_main}>
-        <div className={styles.links_container}>
-            <Link className={styles.link} href={'/equipamentos'}><p>EQUIPAMENTOS</p></Link>
-            <Link className={styles.link} href={'/encantamentos'}><p>ENCANTAMENTOS</p></Link>
-            <Link className={styles.link} href={'/mobs'}><p>MOBS</p></Link>
-            <Link className={styles.link} href={'/feedback'}><p>FEEDBACK</p></Link>
-            <Link className={styles.link} href={'/membros'}><p>CRIADORES</p></Link>
-        </div>
-        <Link href={'/'}><img className={styles.logo} src="/logo-minecraft.svg" alt="logo" width={175} height={40} /></Link>
-        <div className={styles.teste}>
-            <Link className={styles.battle} href={'/batalha'}><span className={styles.battle_tittle}>Batalhe agora</span></Link>
-        </div>
+const Header = () => {
+    const [headerResponsive, setHeaderResponsive] = useState(false);
+    return (
+        <header className={styles.header_main}>
+            <i onClick={() => setHeaderResponsive(!headerResponsive)} className={styles.treeBars}><LiaBarsSolid /></i>
 
-    </header>
-);
+            {
+                headerResponsive && (
+                    <div className={styles.containerResponsive}>
+                        <Link className={styles.linkResponsive} href={'/equipamentos'}><p>EQUIPAMENTOS</p></Link>
+                        <Link className={styles.linkResponsive} href={'/encantamentos'}><p>ENCANTAMENTOS</p></Link>
+                        <Link className={styles.linkResponsive} href={'/mobs'}><p>MOBS</p></Link>
+                        <Link className={styles.linkResponsive} href={'/feedback'}><p>FEEDBACK</p></Link>
+                        <Link className={styles.linkResponsive} href={'/membros'}><p>CRIADORES</p></Link>
+                        <Link className={`${styles.linkResponsive} ${styles.battleNow}`} href={'/batalha'}><span className={styles.battle_tittle}>Batalhe agora</span></Link>
+                    </div>
+                )
+            }
+
+            <div className={styles.links_container}>
+                <Link className={styles.link} href={'/equipamentos'}><p>EQUIPAMENTOS</p></Link>
+                <Link className={styles.link} href={'/encantamentos'}><p>ENCANTAMENTOS</p></Link>
+                <Link className={styles.link} href={'/mobs'}><p>MOBS</p></Link>
+                <Link className={styles.link} href={'/feedback'}><p>FEEDBACK</p></Link>
+                <Link className={styles.link} href={'/membros'}><p>CRIADORES</p></Link>
+            </div>
+            <Link className={styles.logoAii} href={'/'}><img className={styles.logo} src="/logo-minecraft.svg" alt="logo" width={175} height={40} /></Link>
+            <div className={styles.teste}>
+                <Link className={styles.battle} href={'/batalha'}><span className={styles.battle_tittle}>Batalhe agora</span></Link>
+            </div>
+
+        </header>
+    )
+};
 
 export default Header;
