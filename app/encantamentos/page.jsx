3d9d90ea29b669cecdd8c8fb942f61647a1cb199
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2023-12-14 14:56:00 -0300

    Update bookPopUp and encantamentos page styles

--- app/encantamentos/page.jsx
+++ app/encantamentos/page.jsx
@@ -197,6 +197,7 @@ const Page_de_encantamentos = () => {
             router.push("/encantamentos");
             setDados(dados.filter((encantamento) => encantamento.id !== id));
             sendErrorMsg("Encantamento deletado com sucesso");
+            setPopUpOpenBook(null)
         } catch (error) {
             console.log(error);
         }
@@ -242,7 +243,14 @@ const Page_de_encantamentos = () => {
                                 <form className={styles.form_inputs} onSubmit={(e) => handleSend(e, 'dano')}>
                                     <input className={styles.inputs} value={titulo} onChange={(e) => setTitulo(e.target.value)} type="text" placeholder='Titulo' />
                                     <input className={styles.inputs} value={descricao} onChange={(e) => setDescricao(e.target.value)} type="text" placeholder='Descricao' />
-                                    <input className={styles.inputs} value={tipoEncanto} onChange={(e) => setTipoEncanto(e.target.value)} type="text" placeholder='Tipo de encantamento' />
+                                    {/* <input className={styles.inputs} value={tipoEncanto} onChange={(e) => setTipoEncanto(e.target.value)} type="text" placeholder='Tipo de encantamento' /> */}
+                                    <select className={styles.select} value={tipoEncanto} onChange={(e) => setTipoEncanto(e.target.value)} type="text" placeholder='Tipo de encantamento' >
+                                        <option className={styles.options} value="espada">Espada</option>
+                                        <option className={styles.options} value="capacete">Capacete</option>
+                                        <option className={styles.options} value="peitoral">Peitoral</option>
+                                        <option className={styles.options} value="calca">Calça</option>
+                                        <option className={styles.options} value="bota">Bota</option>
+                                    </select>
                                     <input className={styles.inputs} value={dano} onChange={(e) => setDano(e.target.value)} type="number" placeholder='Dano' />
                                     <input className={styles.inputs} value={defesa} onChange={(e) => setDefesa(e.target.value)} type="number" placeholder='Defesa' />
                                     <input className={styles.inputs} value={nivel} onChange={(e) => setNivel(e.target.value)} type="number" placeholder='Nivel' />
@@ -264,15 +272,14 @@ const Page_de_encantamentos = () => {
                                                 <ul className={styles.encantamentos_list}>
                                                     {dados.slice(pageIndex * 3, (pageIndex + 1) * 3).map((encantamento) => (
 
-                                                        <li onClick={() => setPopUpOpenBook(encantamento.id)} key={encantamento.id} className={styles.encantamento}>
-                                                            <div className={styles.book}>
-                                                                <div className={styles.pageInfo}>
+                                                        <li key={encantamento.id} className={styles.encantamento}>
+                                                            <div onClick={() => setPopUpOpenBook(encantamento.id)} className={styles.book}>
+                                                                <div className={styles.pageImg}>
                                                                     <img className={styles.bookImg} src="/Enchanted_Book.webp" alt="encantamento1" width={
                                                                         64
                                                                     } height={
                                                                         64
                                                                     } />
-
                                                                 </div>
                                                                 <p>{encantamento.titulo}</p>
                                                             </div>
