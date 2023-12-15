953799ad200d67edb6a26d15fc0336079982c1ad
MagoValdivia28 <felipepedro@aluno.senai.br>
2023-12-15 01:35:56 -0300

    Refactor CSS styles for feedback module

--- app/feedback/page.jsx
+++ app/feedback/page.jsx
@@ -1,4 +1,4 @@
-
+/* Importa o cliente Axios para fazer requisições HTTP */
 'use client';
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -8,17 +8,20 @@ import { useRouter } from "next/navigation";
 import Footer from "../components/footer/footer.jsx";
 
 const feedbackPage = () => {
-
+    /* Estado para armazenar os dados dos feedbacks */
     const [dados, setDados] = useState([]);
 
+    /* Estado para armazenar o ID do feedback que está sendo editado */
     const [edit, setEdit] = useState(null);
 
-
+    /* Hook useEffect para carregar os dados dos feedbacks quando o componente for montado */
     useEffect(() => {
         async function fetchFeedback() {
             try {
+                /* Faz uma requisição GET para a API de feedbacks */
                 const response = await axios.get("/api/feedback");
                 console.log(response);
+                /* Atualiza o estado com os dados obtidos da API */
                 setDados(response.data);
             } catch (error) {
                 console.error("Error fetching data:", error);
@@ -30,14 +33,18 @@ const feedbackPage = () => {
 
     // Enviar Inputs
 
+    /* Estados para armazenar os valores dos inputs do formulário */
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
 
+    /* Função para lidar com o envio do feedback */
     const handleFeedback = async (e) => {
         e.preventDefault();
         try {
+            /* Faz uma requisição POST para a API de feedbacks */
             await axios.post('/api/feedback', { name, email, message });
+            /* Limpa os valores dos inputs após o envio */
             setName('');
             setEmail('');
             setMessage('');
@@ -46,13 +53,16 @@ const feedbackPage = () => {
         }
     };
 
-    //apagar feedback
+    // Apagar feedback
 
+    /* Função para lidar com a exclusão de um feedback */
     const handleDelete = async (id) => {
         console.log('clicou em apagar ' + id);
         const url = `/api/feedback/${id}`;
         try {
+            /* Faz uma requisição DELETE para a API de feedbacks */
             await axios.delete(url);
+            /* Atualiza o estado removendo o feedback excluído */
             const updatedFeedback = dados.filter((feedback) => feedback.id !== id);
             setDados(updatedFeedback);
         } catch (error) {
@@ -61,6 +71,7 @@ const feedbackPage = () => {
         }
     };
 
+    /* Função para preparar o formulário para edição ao clicar no botão Editar */
     const handleEdit = (element) => {
         setEdit(element.id);
         setName(element.nome);
@@ -68,42 +79,50 @@ const feedbackPage = () => {
         setMessage(element.mensagem);
     };
 
+    /* Função para lidar com a edição de um feedback */
     const handleEditar = async () => {
         await axios.put(`/api/feedback/${edit}`, { nome: name, email, mensagem: message });
         setEdit(null);
         setName('');
         setEmail('');
         setMessage('');
+        /* Atualiza os dados após a edição */
         const response = await axios.get('/api/feedback');
         setDados(response.data);
     };
 
+    /* Função para lidar com o envio ou edição de um feedback */
     const handleEnviar = async () => {
         try {
-            await axios.post('/api/feedback', { nome: name, email, mensagem: message });
-            setName('');
-            setEmail('');
-            setMessage('');
-            const response = await axios.get('/api/feedback');
-            setDados(response.data);
+            /* Verifica se está em modo de edição e chama a função correspondente */
+            if (edit) {
+                await handleEditar();
+            } else {
+                await handleEnviar();
+            }
         } catch (error) {
             console.error('Error submitting data:', error);
         }
     };
 
-
     return (
         <>
+            {/* Corpo da página */}
             <body className={styles.body}>
+                {/* Componente de cabeçalho */}
                 <Header />
 
+                {/* Seção principal da página */}
                 <main className={styles.main}>
                     <div className={styles.titles_container}>
+                        {/* Título e subtítulo da seção */}
                         <h2 className={styles.titles}>FEEDBACK!</h2>
                         <p className={styles.subTitles}>Adoramos ouvir seus comentários – veja o que foi sugerido ou poste suas ideias agora. Ajude suas grandes ideias a se tornarem parte do Minecraft! Analisamos suas ideias e comentários todos os dias.</p>
+                        {/* Degrade na imagem */}
                         <div className={styles.imgDegrade}> <br /></div>
                     </div>
                     <div className={styles.form_container}>
+                        {/* Formulário de feedback */}
                         <label className={styles.label} htmlFor="nome">Nome:</label>
                         <input
                             className={styles.input}
@@ -127,6 +146,7 @@ const feedbackPage = () => {
                             value={message}
                             onChange={(e) => setMessage(e.target.value)}
                         />
+                        {/* Botão de enviar ou editar, dependendo do modo */}
                         {
                             edit ? (
                                 <button onClick={handleEditar} className={styles.enviar}>Editar</button>
@@ -137,10 +157,13 @@ const feedbackPage = () => {
                     </div>
 
                     <div className={styles.main_container}>
+                        {/* Navegação da lista de feedbacks */}
                         <nav className={styles.feedback_container}>
                             <ul className={styles.feedback_list}>
                                 <div className={styles.container}>
+                                    {/* Degrade na imagem 2 */}
                                     <div className={styles.imgDegrade2}> <br /></div>
+                                    {/* Exibição da lista de feedbacks */}
                                     <div className={styles.cntn}>
                                         {dados.map((feedback) => (
                                             <div className={styles.card}>
@@ -150,6 +173,7 @@ const feedbackPage = () => {
                                                     <br />
                                                     <p className={styles.feedback_text}><strong>Mensagem:</strong> {feedback.mensagem}</p>
 
+                                                    {/* Botões de exclusão e edição */}
                                                     <div className={styles.container_button}>
                                                         <button
                                                             className={styles.deleteButton}
@@ -164,7 +188,6 @@ const feedbackPage = () => {
                                                         >
                                                             Editar
                                                         </button>
-
                                                     </div>
                                                 </li>
                                             </div>
@@ -172,19 +195,15 @@ const feedbackPage = () => {
                                     </div>
                                 </div>
                             </ul>
-
                         </nav>
                     </div>
                 </main>
 
-
+                {/* Componente de rodapé */}
                 <Footer />
-
             </body>
         </>
-
     );
 };
 
 export default feedbackPage;
-
